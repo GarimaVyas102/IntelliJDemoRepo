5b86e349442f2b59808f3bf1ae889bbca4add8d7
Garima <shanuvyas102@gmail.com>
2021-07-04 18:27:32 +0530

    Initial Commit

---
+++ src/com/demo/Main.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello user");
+        System.out.println("Hello ");
+    }
+}
