2b179c60f0061bf5be1574473485faba4303b642
Garima <shanuvyas102@gmail.com>
2021-07-04 20:17:32 +0530

    changes commit in Practice1 methods

--- src/com/demo/Practice1.java
+++ src/com/demo/Practice1.java
@@ -5,4 +5,7 @@ class Practice1 {
         System.out.println("This is Practical Group ");
         System.out.println("This is Notification Demo  ");
     }
+    void method1(){
+        System.out.println(" This is a changes used to check notifications  ");
+    }
 }
